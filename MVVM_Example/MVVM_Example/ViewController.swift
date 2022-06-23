85034047ae74b02176af0ff0bb09615c0168e09c
Lee McCormick <lmccormick@m3ts.com>
2022-06-23 13:31:05 -0500

    added the MVVM

--- MVVM_Example/MVVM_Example/ViewController.swift
+++ MVVM_Example/MVVM_Example/ViewController.swift
@@ -7,6 +7,26 @@
 
 import UIKit
 
+// MARK: - ObservableObject --> Take generic and listen to the change and bind when the value is changed, when only 1 listener and have not been use in this project, but reference from https://www.youtube.com/watch?v=sLHVxnRS75w
+final class ObservableObject<T> {
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    private var listener: ((T) -> Void)?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ listener: @escaping (T) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
+
 // MARK: - Obserable --> Take generic and listen to the change and bind when the value is changed.
 class Observable<T> {
     var value: T? {
@@ -105,6 +125,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 }
 
+// MARK: - MVVM --> can be implement in many way in Switf forexample ObservableObject, Combine, Delegate,  Key-Value-Observing or RxSwift
+/* Note : Making the ViewModel Dynamic
+
+ Because your data will change, you need to make your ViewModel dynamic.
+
+ What this means is that when the Model changes, ViewModel should change its public property values; it would propagate the change back to the view, which is the one that will update the UI.
+
+ There are a lot of ways to do this.
+
+ When Model changes, ViewModel gets notified first.
+
+ You need some mechanism to propagate what changes up to the View.
+
+ Some of the options include RxSwift, which is a pretty large library and takes some time to get used to.
+
+ ViewModel could be firing NSNotifications on each property value change, but this adds a lot of code that needs additional handling, such as subscribing to notifications and unsubscribing when the view gets deallocated.
+
+ Key-Value-Observing (KVO) is another option, but users will confirm that its API is not fancy.
+
+ In this tutorial, you’ll use Swift generics and closures, which are nicely described in the Bindings, Generics, Swift and MVVM article.
+ */
+
 /* Source :
  https://www.youtube.com/watch?v=iI0LabCYZJo
+ https://www.youtube.com/watch?v=sLHVxnRS75w
+ https://www.youtube.com/watch?v=7HKi96v4X2A
  */
